2aa4b54464020d908a9e7222ee0f3ff4dd9188f8
pgcrism <pgcrism>
2012-02-10 23:24:42 +0000

    [FIX] include once only.

--- src/spec/C/ecli_c.h
+++ src/spec/C/ecli_c.h
@@ -1,17 +1,20 @@
+#ifndef ECLI_C_H
+#define ECLI_C_H
 #include <ecli_c_spec.h>
 
 struct ecli_c_value {
-	SQLLEN	length; /* buffer_length */
-	SQLLEN	length_indicator; /* length of data in the buffer */
+	long	length; /* buffer_length */
+	long	length_indicator;
 	char	value[1]; /* beginning of buffer*/
 };
 
 struct ecli_c_array_value {
-	SQLLEN	length; /* length of a single element buffer*/
+	long	length; /* length of a single element */
 	long 	count;  /* count of elements */
 	char	buffer[1]; /* beginning of buffer*/
 	/* buffer is organized like this
-		SQLLEN length_indicator [count] -- array of actual element length
-		char values [count][length]   -- array of element buffer
+		long length_indicator [count]
+		char values [count][length]
 	*/
 };
+#endif
